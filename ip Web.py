7923d48debe332e7f635bd8323f73b7e1b046924
HEKSOS <86925042+HEKSOS555@users.noreply.github.com>
2021-07-06 12:34:21 +0200

    Add files via upload

---
+++ ip Web.py
@@ -0,0 +1,24 @@
+import os 
+os.system("clear")
+import socket 
+import sys
+
+print("")
+print("=======================")
+print("    iP Website")
+print("=======================")
+print(" YouTube : MR.HEKSOS ")
+print("=======================")
+print("")
+
+print("inter Your Target: ")
+hostname = input()
+ip=socket.gethostbyname(hostname)
+print('Host name is:  ',hostname,'  ' 'Target ip is:  ',ip)
+
+
+print("")
+print("=================")
+print("py_HEKSOS")
+print("=================")
+print("")
